323f9f81b2dab8c9e78586f2942f41efbde5671d
Shubh Chaturvedi <89243211+ShubhChaturvedi@users.noreply.github.com>
2023-11-13 05:57:35 +0000

    DEV: Variables in js

---
+++ 001_basics/variables.js
@@ -0,0 +1,24 @@
+const accountID = 123;
+let accountName = "Shubh"
+var accountPassword = "Shubh@123"
+accountEmail = "shubh@gmail.com" // can possible in js but not preferred
+
+let account;
+
+/*
+var keyword: not use in 2023 because it is deprecated because of its non responsiveness for block level scope
+and punctional level scope
+*/
+
+
+// accountID = 143 // not allowed
+
+console.log(accountID)
+
+console.table([
+    accountID,
+    accountName,
+    accountPassword,
+    accountEmail,
+    account
+])
